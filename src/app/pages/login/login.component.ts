9913ec104d13e3b28d786f4ae74f17ecfb72a6a4
Yasin Ayyildiz <ayyildiz.yasinenes03@gmail.com>
2025-05-14 21:17:50 +0200

    Finished login-frontend, adjusted colors of header/footer and homepage

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,65 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+    FormBuilder,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
     selector: 'app-login',
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        MatCardModule,
+        MatInputModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+    ],
     templateUrl: './login.component.html',
-    styleUrl: './login.component.scss',
+    styleUrls: ['./login.component.scss'],
+    animations: [
+        trigger('fadeInUp', [
+            transition(':enter', [
+                style({ opacity: 0, transform: 'translateY(5px)' }),
+                animate(
+                    '200ms ease-out',
+                    style({ opacity: 1, transform: 'translateY(0)' }),
+                ),
+            ]),
+        ]),
+    ],
 })
-export class LoginComponent {}
+export class LoginComponent {
+    loginForm: FormGroup;
+    submitted = false;
+
+    constructor(private fb: FormBuilder) {
+        this.loginForm = this.fb.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required],
+        });
+    }
+
+    onSubmit() {
+        this.submitted = true;
+        if (this.loginForm.valid) {
+            const { username, password } = this.loginForm.value;
+            console.log('Login:', username, password);
+        }
+    }
+
+    get username() {
+        return this.loginForm.get('username');
+    }
+
+    get password() {
+        return this.loginForm.get('password');
+    }
+}
